5081701c29decc298325e9188780cb9010627a19
lovasoa3 <hei.lovasoa.3@gmail.com>
2023-12-04 20:47:09 +0100

    connection base de donnee et modelisation de compte

---
+++ tdprog3 (1)/tdprog3/src/main/java/model/Devise.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Devise {
+    private int id_devise;
+    private String type_devise;
+}
